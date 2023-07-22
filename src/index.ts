544516691077d78327ab3412b78437324ed30caa
Dipu <dipuchaurasiya91@gmail.com>
2023-07-22 17:17:06 +0545

    cors updated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { NextFunction, Response } from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
 import connectDB from "./server";
 import errorHandler from "./utils/errorHandler";
 import userRoutes from "./routes/userRoute";
@@ -12,37 +12,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-if (process.env.NODE_ENV === "development") {
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://cinemaa.vercel.app");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, PATCH"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
-} else if (process.env.NODE_ENV === "production") {
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://cinemaa.vercel.app");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, PATCH"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, authorization"
-    );
-
-    next();
-  });
-}
+app.use(cors());
 
 app.use("/user", userRoutes);
 app.use("/media", mediaRoutes);
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(errorHandler);
 
