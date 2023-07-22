2524784a44e71fe644119cfe9c3776fcf2519239
Dipu <dipuchaurasiya91@gmail.com>
2023-07-22 16:21:29 +0545

    cors updated

--- src/index.ts
+++ src/index.ts
@@ -12,27 +12,32 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "development") {
-  app.use((req, res, next: NextFunction) => {
+  app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, PUT, DELETE, PATCH"
     );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      ["Content-Type", "authorization"] // Pass multiple headers as an array
+    );
     next();
   });
 } else if (process.env.NODE_ENV === "production") {
-  app.use((req, res, next: NextFunction) => {
+  app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "https://cinemaa.vercel.app");
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, PUT, DELETE, PATCH"
     );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Headers", [
+      "Content-Type",
+      "authorization",
+    ]);
     next();
   });
 }
-
 app.use("/user", userRoutes);
 app.use("/media", mediaRoutes);
 
