cc82b0a4628525c55b6e627581e721d702ce818e
Dipu <dipuchaurasiya91@gmail.com>
2023-08-25 10:54:40 +0545

    telegram removed

--- src/index.ts
+++ src/index.ts
@@ -26,37 +26,37 @@ app.use("/media", mediaRoutes);
 
 const port = process.env.PORT! || 3000;
 
-const TELEGRAM_PORT = Number(process.env.TELEGRAM_PORT!) || 3001;
+// const TELEGRAM_PORT = Number(process.env.TELEGRAM_PORT!) || 3001;
 
 app.use(errorHandler);
 
-const token: string = process.env.TELEGRAM_BOT_TOKEN!;
-const options = {
-  webHook: {
-    port: TELEGRAM_PORT,
-  },
-};
-const bot = new TelegramBot(token, options);
-const BASEURL = process.env.BASE_URL;
-bot.setWebHook(`${BASEURL}/bot${token}`);
+// const token: string = process.env.TELEGRAM_BOT_TOKEN!;
+// const options = {
+//   webHook: {
+//     port: TELEGRAM_PORT,
+//   },
+// };
+// const bot = new TelegramBot(token, options);
+// const BASEURL = process.env.BASE_URL;
+// bot.setWebHook(`${BASEURL}/bot${token}`);
 
-app.post(`/bot${token}`, (req, res) => {
-  const message: Message = req.body.message;
-  if (message) {
-    const myBot = new MyTelegrambot(message, bot);
-    myBot
-      .AnyMessag()
-      .then(() => {
-        res.status(200).send("Message sent successfully");
-      })
-      .catch((error) => {
-        console.error("Error sending message:", error);
-        res.status(500).send("Error sending message");
-      });
-  } else {
-    res.status(200).send("No message received");
-  }
-});
+// app.post(`/bot${token}`, (req, res) => {
+//   const message: Message = req.body.message;
+//   if (message) {
+//     const myBot = new MyTelegrambot(message, bot);
+//     myBot
+//       .AnyMessag()
+//       .then(() => {
+//         res.status(200).send("Message sent successfully");
+//       })
+//       .catch((error) => {
+//         console.error("Error sending message:", error);
+//         res.status(500).send("Error sending message");
+//       });
+//   } else {
+//     res.status(200).send("No message received");
+//   }
+// });
 
 connectDB()
   .then(() => {
